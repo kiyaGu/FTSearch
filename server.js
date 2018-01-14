813758143179cab55e649ef7712feadf144cf620
KiyaG <hamilee.kiya@gmail.com>
2018-01-14 20:07:36 +0000

    implemented forward pagination with only CSS and HTML

--- server.js
+++ server.js
@@ -29,12 +29,14 @@ app.get("/", (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                queryString: "latest",
+                queryString: "",
                 queryContext: {
                     curations: ["ARTICLES"]
                 },
                 resultContext: {
                     maxResults: 9,
+                    offset: 0,
+                    contextual: true,
                     aspects: [
                         "title",
                         "summary",
@@ -85,7 +87,7 @@ app.post("/search", (req, res) => {
                     curations: ["ARTICLES"]
                 },
                 resultContext: {
-                    maxResults: 10,
+                    maxResults: 20,
                     aspects: [
                         "title",
                         "summary",
@@ -107,6 +109,10 @@ app.post("/search", (req, res) => {
             let articles = [];
             response.results[0].results.forEach(article => {
                 articles.push({
+                    currentPage: 1,
+                    totalPages: response.results[0].indexCount < 4000 ?
+                        response.results[0].indexCount :
+                        4000,
                     keyword: response.query.queryString,
                     title: article.title.title,
                     author: article.editorial.byline,
@@ -123,6 +129,63 @@ app.post("/search", (req, res) => {
             res.render("articles", { articles: articles });
         });
 });
+app.post("/page", (req, res) => {
+    fetch("http://api.ft.com/content/search/v1", {
+            method: "POST",
+            headers: {
+                "X-Api-Key": "59cbaf20e3e06d3565778e7bbfc218a2dde8436ba6af7ed77d5afa7e",
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                queryString: req.fields.currentKeyword,
+                queryContext: {
+                    curations: ["ARTICLES"]
+                },
+                resultContext: {
+                    maxResults: 20,
+                    offset: req.fields.currentPageNumber * 20,
+                    aspects: [
+                        "title",
+                        "summary",
+                        "editorial",
+                        "location",
+                        "metadata",
+                        "lifecycle"
+                    ]
+                }
+            })
+        })
+        .then(function(response) {
+            if (response.status >= 400) {
+                throw new Error("The resource is not found");
+            }
+            return response.json();
+        })
+        .then(function(response) {
+            let articles = [];
+            response.results[0].results.forEach(article => {
+                articles.push({
+                    currentPage: parseInt(req.fields.currentPageNumber) + 1,
+                    totalPages: response.results[0].indexCount < 4000 ?
+                        response.results[0].indexCount :
+                        4000,
+                    keyword: response.query.queryString,
+                    title: article.title.title,
+                    author: article.editorial.byline,
+                    excerpt: article.summary.excerpt,
+                    publicationDate: moment(
+                        article.lifecycle.initialPublishDateTime
+                    ).format("dddd, Do MMMM, YYYY"),
+                    link: article.location.uri,
+                    tag: article.metadata.primarySection.term.name,
+                    tagLink: `https://www.ft.com/${article.metadata.primarySection.term.name.toLowerCase()}`
+                });
+            });
+
+            res.render("articles", { articles: articles });
+        });
+});
+
 app.listen(3000, () => {
     console.log("server running at localhost:3000");
 });
