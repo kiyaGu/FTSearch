7cb87ee4b4c0c4a419176c846d7e583c7e20925e
KiyaG <hamilee.kiya@gmail.com>
2018-01-15 15:52:41 +0000

    refactored the next pagination method by creating new createArticleEntry and setPaginationElements

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ if (
     document.dispatchEvent(new CustomEvent("o.DOMContentLoaded"));
 
     /*==========================
-                                                ajax request
-                                                      ===================================*/
+                                                                              ajax request
+                                                                                    ===================================*/
     function Ajax_request() {
         if (window.XMLHttpRequest) {
             // Mozilla, Safari, ...
@@ -28,8 +28,8 @@ if (
         return xmlhttp;
     }
     /*==========================
-                    functions for creating DOM elements
-                    ===================================*/
+                                                  functions for creating DOM elements
+                                                  ===================================*/
     //to create DOM elements
     let createElementFunc = function(elementType) {
         return document.createElement(elementType);
@@ -42,113 +42,118 @@ if (
     let setElementAttribute = function(element, attribute, value) {
         element.setAttribute(attribute, value);
     };
+    let createArticleEntry = (article, ul) => {
+        let li = createElementFunc("li");
+        let articleTag = createElementFunc("article");
+
+        setElementAttribute(articleTag, "class", "o-teaser o-teaser--has-image");
+        setElementAttribute(articleTag, "data-o-component", "o-teaser");
+
+        let div1 = createElementFunc("div");
+        setElementAttribute(div1, "class", "o-teaser__image-container");
+
+        let div2 = createElementFunc("div");
+        setElementAttribute(
+            div2,
+            "class",
+            "o-teaser__image-placeholder image-placeholder"
+        );
+
+        let img = createElementFunc("img");
+        img.src =
+            "https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fim.ft-static.com%2Fcontent%2Fimages%2Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img?url=http%253A%252F%252Fim.ft-static.com%252Fcontent%252Fimages%252Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img&source=o-teaser-demo&width=400&height=400&fit=cover&format=auto&quality=medium ";
+
+        setElementAttribute(img, "class", "o-teaser__image");
+        setElementAttribute(img, "alt", "article image");
+        let div3 = createElementFunc("div");
+        setElementAttribute(div3, "class", "o-teaser__content");
+        let div4 = createElementFunc("div");
+        setElementAttribute(div4, "class", "o-teaser__meta");
+        let h31 = createElementFunc("h3");
+        setElementAttribute(h31, "class", "article__tag");
+        let a1 = createElementFunc("a");
+        setElementAttribute(a1, "href", `${article.tagLink}`);
+        a1.appendChild(createTextContentFunc(article.tag));
+        let h32 = createElementFunc("h3");
+        setElementAttribute(h32, "class", "o-teaser__heading ");
+        let a2 = createElementFunc("a");
+        setElementAttribute(a2, "href", `${article.link}`);
+        a2.appendChild(createTextContentFunc(article.title));
+        let p1 = createElementFunc("p");
+        setElementAttribute(p1, "class", "o-teaser__standfirst");
+        p1.appendChild(createTextContentFunc(article.excerpt));
+        let p2 = createElementFunc("p");
+        if (article.author) {
+            p2.appendChild(createTextContentFunc(article.author));
+        } else {
+            //instead of undefined will be ""
+            p2.appendChild(createTextContentFunc(""));
+        }
+
+        let time = createElementFunc("time");
+        time.appendChild(createTextContentFunc(article.publicationDate));
+
+        ul.appendChild(li);
+        li.appendChild(articleTag);
+        articleTag.appendChild(div1);
+        div1.appendChild(div2);
+        div2.appendChild(img);
+
+        articleTag.appendChild(div3);
+        div3.appendChild(div4);
+        div4.appendChild(h31);
+        h31.appendChild(a1);
+        div3.appendChild(h32);
+        h32.appendChild(a2);
+        div3.appendChild(p1);
+
+        div3.appendChild(p2);
+
+        div3.appendChild(time);
+    };
+    let setPaginationElements = page => {
+        let nexPageNumber = page.nextPage;
+        let previousPage = page.previousPage;
+        let currentPage = page.currentPage;
+        let keyword = page.keyword;
+
+        document.getElementById("currentPage-Number").innerHTML = page.currentPage;
+        document.getElementById(
+            "previousPage"
+        ).href = `?q=${keyword}&page=${previousPage}`;
+        document.getElementById(
+            "nextPage"
+        ).href = `?q=${keyword}&page=${nexPageNumber}`;
+        history.replaceState("", "", `?q=${keyword}&page=${currentPage}`);
+    };
     let nextPageButton = document.getElementById("nextPage");
 
     if (nextPageButton !== null) {
         document.getElementById("nextPage").addEventListener("click", e => {
             var query = document.getElementById("nextPage").getAttribute("href");
-            var url = `/ajaxSearch${query}`;
+            var url = `/search${query}`;
             e.preventDefault();
+
             if (self.fetch) {
                 fetch(url, {
-                        method: "GET"
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
                     })
                     .then(res => res.json())
                     .catch(error => console.error("Error:", error))
                     .then(response => {
                         let ul = document.getElementById("list-of--results");
+                        //removing the existing result
                         while (ul.firstChild) {
                             ul.removeChild(ul.firstChild);
                         }
 
                         response.articles.forEach(article => {
-                            let li = createElementFunc("li");
-                            let articleTag = createElementFunc("article");
-
-                            setElementAttribute(
-                                articleTag,
-                                "class",
-                                "o-teaser o-teaser--has-image"
-                            );
-                            setElementAttribute(articleTag, "data-o-component", "o-teaser");
-
-                            let div1 = createElementFunc("div");
-                            setElementAttribute(div1, "class", "o-teaser__image-container");
-
-                            let div2 = createElementFunc("div");
-                            setElementAttribute(
-                                div2,
-                                "class",
-                                "o-teaser__image-placeholder image-placeholder"
-                            );
-
-                            let img = createElementFunc("img");
-                            img.src =
-                                "https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fim.ft-static.com%2Fcontent%2Fimages%2Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img?url=http%253A%252F%252Fim.ft-static.com%252Fcontent%252Fimages%252Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img&source=o-teaser-demo&width=400&height=400&fit=cover&format=auto&quality=medium ";
-
-                            setElementAttribute(img, "class", "o-teaser__image");
-                            setElementAttribute(img, "alt", "article image");
-                            let div3 = createElementFunc("div");
-                            setElementAttribute(div3, "class", "o-teaser__content");
-                            let div4 = createElementFunc("div");
-                            setElementAttribute(div4, "class", "o-teaser__meta");
-                            let h31 = createElementFunc("h3");
-                            setElementAttribute(h31, "class", "article__tag");
-                            let a1 = createElementFunc("a");
-                            setElementAttribute(a1, "href", `${article.tagLink}`);
-                            a1.appendChild(createTextContentFunc(article.tag));
-                            let h32 = createElementFunc("h3");
-                            setElementAttribute(h32, "class", "o-teaser__heading ");
-                            let a2 = createElementFunc("a");
-                            setElementAttribute(a2, "href", `${article.link}`);
-                            a2.appendChild(createTextContentFunc(article.title));
-                            let p1 = createElementFunc("p");
-                            setElementAttribute(p1, "class", "o-teaser__standfirst");
-                            p1.appendChild(createTextContentFunc(article.excerpt));
-                            let p2 = createElementFunc("p");
-                            if (article.author) {
-                                p2.appendChild(createTextContentFunc(article.author));
-                            } else {
-                                //instead of undefined will be ""
-                                p2.appendChild(createTextContentFunc(""));
-                            }
-
-                            let time = createElementFunc("time");
-                            time.appendChild(createTextContentFunc(article.publicationDate));
-
-                            ul.appendChild(li);
-                            li.appendChild(articleTag);
-                            articleTag.appendChild(div1);
-                            div1.appendChild(div2);
-                            div2.appendChild(img);
-
-                            articleTag.appendChild(div3);
-                            div3.appendChild(div4);
-                            div4.appendChild(h31);
-                            h31.appendChild(a1);
-                            div3.appendChild(h32);
-                            h32.appendChild(a2);
-                            div3.appendChild(p1);
-
-                            div3.appendChild(p2);
-
-                            div3.appendChild(time);
+                            createArticleEntry(article, ul);
                         });
-
-                        let nexPageNumber = response.articles[0].nextPage;
-                        let previousPage = response.articles[0].previousPage;
-                        let currentPage = response.articles[0].currentPage;
-                        let keyword = response.articles[0].keyword;
-
-                        document.getElementById("currentPage-Number").innerHTML =
-                            response.articles[0].currentPage;
-                        document.getElementById(
-                            "previousPage"
-                        ).href = `?q=${keyword}&page=${previousPage}`;
-                        document.getElementById(
-                            "nextPage"
-                        ).href = `?q=${keyword}&page=${nexPageNumber}`;
-                        history.replaceState("", "", `?q=${keyword}&page=${currentPage}`);
+                        setPaginationElements(response.articles[0]);
                     });
             } else {
                 let request = Ajax_request();
@@ -168,107 +173,9 @@ if (
                                 }
 
                                 response.articles.forEach(article => {
-                                    let li = createElementFunc("li");
-                                    let articleTag = createElementFunc("article");
-
-                                    setElementAttribute(
-                                        articleTag,
-                                        "class",
-                                        "o-teaser o-teaser--has-image"
-                                    );
-                                    setElementAttribute(
-                                        articleTag,
-                                        "data-o-component",
-                                        "o-teaser"
-                                    );
-
-                                    let div1 = createElementFunc("div");
-                                    setElementAttribute(
-                                        div1,
-                                        "class",
-                                        "o-teaser__image-container"
-                                    );
-
-                                    let div2 = createElementFunc("div");
-                                    setElementAttribute(
-                                        div2,
-                                        "class",
-                                        "o-teaser__image-placeholder image-placeholder"
-                                    );
-
-                                    let img = createElementFunc("img");
-                                    img.src =
-                                        "https://www.ft.com/__origami/service/image/v2/images/raw/http%3A%2F%2Fim.ft-static.com%2Fcontent%2Fimages%2Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img?url=http%253A%252F%252Fim.ft-static.com%252Fcontent%252Fimages%252Fa60ae24b-b87f-439c-bf1b-6e54946b4cf2.img&source=o-teaser-demo&width=400&height=400&fit=cover&format=auto&quality=medium ";
-
-                                    setElementAttribute(img, "class", "o-teaser__image");
-                                    setElementAttribute(img, "alt", "article image");
-                                    let div3 = createElementFunc("div");
-                                    setElementAttribute(div3, "class", "o-teaser__content");
-                                    let div4 = createElementFunc("div");
-                                    setElementAttribute(div4, "class", "o-teaser__meta");
-                                    let h31 = createElementFunc("h3");
-                                    setElementAttribute(h31, "class", "article__tag");
-                                    let a1 = createElementFunc("a");
-                                    setElementAttribute(a1, "href", `${article.tagLink}`);
-                                    a1.appendChild(createTextContentFunc(article.tag));
-                                    let h32 = createElementFunc("h3");
-                                    setElementAttribute(h32, "class", "o-teaser__heading ");
-                                    let a2 = createElementFunc("a");
-                                    setElementAttribute(a2, "href", `${article.link}`);
-                                    a2.appendChild(createTextContentFunc(article.title));
-                                    let p1 = createElementFunc("p");
-                                    setElementAttribute(p1, "class", "o-teaser__standfirst");
-                                    p1.appendChild(createTextContentFunc(article.excerpt));
-                                    let p2 = createElementFunc("p");
-                                    if (article.author) {
-                                        p2.appendChild(createTextContentFunc(article.author));
-                                    } else {
-                                        //instead of undefined will be ""
-                                        p2.appendChild(createTextContentFunc(""));
-                                    }
-
-                                    let time = createElementFunc("time");
-                                    time.appendChild(
-                                        createTextContentFunc(article.publicationDate)
-                                    );
-
-                                    ul.appendChild(li);
-                                    li.appendChild(articleTag);
-                                    articleTag.appendChild(div1);
-                                    div1.appendChild(div2);
-                                    div2.appendChild(img);
-
-                                    articleTag.appendChild(div3);
-                                    div3.appendChild(div4);
-                                    div4.appendChild(h31);
-                                    h31.appendChild(a1);
-                                    div3.appendChild(h32);
-                                    h32.appendChild(a2);
-                                    div3.appendChild(p1);
-
-                                    div3.appendChild(p2);
-
-                                    div3.appendChild(time);
+                                    createArticleEntry(article, ul);
                                 });
-
-                                let nexPageNumber = response.articles[0].nextPage;
-                                let previousPage = response.articles[0].previousPage;
-                                let currentPage = response.articles[0].currentPage;
-                                let keyword = response.articles[0].keyword;
-
-                                document.getElementById("currentPage-Number").innerHTML =
-                                    response.articles[0].currentPage;
-                                document.getElementById(
-                                    "previousPage"
-                                ).href = `?q=${keyword}&page=${previousPage}`;
-                                document.getElementById(
-                                    "nextPage"
-                                ).href = `?q=${keyword}&page=${nexPageNumber}`;
-                                history.replaceState(
-                                    "",
-                                    "",
-                                    `?q=${keyword}&page=${currentPage}`
-                                );
+                                setPaginationElements(response.articles[0]);
                             } else {
                                 alert(
                                     "An error occurred during your request: " +
@@ -294,14 +201,17 @@ if (
     if (previousPageButton !== null) {
         document.getElementById("previousPage").addEventListener("click", e => {
             let query = document.getElementById("previousPage").getAttribute("href");
-            let url = `/ajaxSearch${query}`;
+            let url = `/search${query}`;
 
             e.preventDefault();
 
             //if fetch is supported by the client browser
             if (self.fetch) {
                 fetch(url, {
-                        method: "GET"
+                        method: "GET",
+                        headers: {
+                            "Content-Type": "application/json"
+                        }
                     })
                     .then(res => res.json())
                     .catch(error => console.error("Error:", error))
--- server.js
+++ server.js
@@ -142,80 +142,16 @@ app.get("/search", (req, res) => {
                 });
             });
 
-            res.render("articles", { articles: articles });
-        });
-});
-app.get("/ajaxSearch", (req, res) => {
-    let keyword = req.query.q;
-    let currentPage = req.query.page ? parseInt(req.query.page) : 1;
-
-    let offset = 0;
-    if (currentPage >= 1 && currentPage <= 200) {
-        offset = (currentPage - 1) * 20;
-    }
-    fetch("http://api.ft.com/content/search/v1", {
-            method: "POST",
-            headers: {
-                "X-Api-Key": "59cbaf20e3e06d3565778e7bbfc218a2dde8436ba6af7ed77d5afa7e",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({
-                queryString: keyword,
-                queryContext: {
-                    curations: ["ARTICLES"]
-                },
-                resultContext: {
-                    maxResults: 20,
-                    offset: offset,
-                    aspects: [
-                        "title",
-                        "summary",
-                        "editorial",
-                        "location",
-                        "metadata",
-                        "lifecycle"
-                    ]
-                }
-            })
-        })
-        .then(function(response) {
-            if (response.status >= 400) {
-                throw new Error("The resource is not found");
+            let contentType = req.headers["content-type"];
+            if (contentType == "application/json") {
+                //for ajax and fetch implementations of pagination
+                res.json({ articles: articles });
+            } else {
+                res.render("articles", { articles: articles });
             }
-            return response.json();
-        })
-        .then(function(response) {
-            let articles = [];
-            let totalNumberOfPages =
-                response.results[0].indexCount < 4000 ?
-                response.results[0].indexCount > 20 ?
-                Math.ceil(response.results[0].indexCount / 20) :
-                1 :
-                200;
-            response.results[0].results.forEach(article => {
-                articles.push({
-                    previousPage: currentPage - 1 >= 1 ? currentPage - 1 : 1,
-                    currentPage: currentPage,
-                    nextPage: currentPage + 1 > totalNumberOfPages ?
-                        currentPage :
-                        currentPage + 1,
-                    totalNumberOfPages: totalNumberOfPages,
-                    keyword: response.query.queryString,
-                    title: article.title.title,
-                    author: article.editorial.byline,
-                    excerpt: article.summary.excerpt,
-                    publicationDate: moment(
-                        article.lifecycle.initialPublishDateTime
-                    ).format("dddd, Do MMMM, YYYY"),
-                    link: article.location.uri,
-                    tag: article.metadata.primarySection.term.name,
-                    tagLink: `https://www.ft.com/${article.metadata.primarySection.term.name.toLowerCase()}`
-                });
-            });
-
-            res.json({ articles: articles });
         });
 });
+
 app.listen(3000, () => {
     console.log("server running at localhost:3000");
 });
