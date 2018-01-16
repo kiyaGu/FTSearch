e80202589f1f9233d878739805ffa399781acc5f
KiyaG <hamilee.kiya@gmail.com>
2018-01-16 11:35:58 +0000

    to avoid the removal of the current view if there is no response from the server wrapped the code that updates the view with if(response)

--- assets/js/updateThePage.js
+++ assets/js/updateThePage.js
@@ -2,16 +2,18 @@ const createArticleEntry = require("./createArticleEntryDOMElements");
 const setPaginationElements = require("./setPaginationElementsValue");
 
 module.exports = function(response) {
-    // get the <ul> that holds all the dispalyed articles
-    let ul = document.getElementById("list-of--results");
-    //remove all the current childer nodes
-    while (ul.firstChild) {
-        ul.removeChild(ul.firstChild);
+    if (response) {
+        // get the <ul> that holds all the dispalyed articles
+        let ul = document.getElementById("list-of--results");
+        //remove all the current childer nodes
+        while (ul.firstChild) {
+            ul.removeChild(ul.firstChild);
+        }
+        // update the <ul> with the new  list of artcles
+        response.articles.forEach(article => {
+            createArticleEntry(article, ul);
+        });
+        //modify the page number displayed
+        setPaginationElements(response.articles[0]);
     }
-    // update the <ul> with the new  list of artcles
-    response.articles.forEach(article => {
-        createArticleEntry(article, ul);
-    });
-    //modify the page number displayed
-    setPaginationElements(response.articles[0]);
 };
